58669229ae66a0f2dda447e01af022cf0956a148
nithinmohan <nithinmohan94@gmail.com>
2015-10-23 01:37:49 +0530

    basic rest and db added

--- app.js
+++ app.js
@@ -1,16 +1,108 @@
 // Load the http module to create an http server.
 var http = require('http');
+var express =require('express');
+var app=express();
+var bodyParser= require('body-parser');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  if(request.url=='/new')
-  response.end("Hello World\n");
-  response.end("Hell22o World\n");
+var mongoose=require('mongoose');
+mongoose.connect('localhost:27017/test');
+
+var Bear     = require('./models/bear');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//ip and ports
+var ip = process.env.IP || '192.168.178.1',
+    port = process.env.PORT || 8080;
+
+//Router
+
+var router=express.Router();
+
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
 });
 
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+        
+        var bear = new Bear();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Bear created!' });
+        });
+        
+    }).get(function(req, res) {
+        Bear.find(function(err, bears) {
+            if (err)
+                res.send(err);
+
+            res.json(bears);
+        });
+    });
+
+
+router.route('/bears/:bear_id')
+
+    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            if (err)
+                res.send(err);
+            res.json(bear);
+        });
+    })
+    .put(function(req, res) {
+
+        // use our bear model to find the bear we want
+        Bear.findById(req.params.bear_id, function(err, bear) {
+
+            if (err)
+                res.send(err);
+
+            bear.name = req.body.name;  // update the bears info
+
+            // save the bear
+            bear.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Bear updated!' });
+            });
+
+        });
+    })
+    .delete(function(req, res) {
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function(err, bear) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
+
+
+router.get('/',function(req,res){
+  res.json({msg:'yup'});
+})
+
+app.use('/',router);
+
 // Listen on port 8000, IP defaults to 127.0.0.1
-var ip = process.env.IP || '192.168.178.1',
-    port = process.env.PORT || 3000;
-server.listen(port,ip);
+
+app.listen(port);
